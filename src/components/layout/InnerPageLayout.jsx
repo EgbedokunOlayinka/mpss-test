26f7526330a84bc8dae6b6cb479256b9594fb3d0
Egbedokun Olayinka <egbedokunolayinka@gmail.com>
2021-03-23 13:26:41 +0100

    mobile nav bug fixing

--- src/components/layout/InnerPageLayout.jsx
+++ src/components/layout/InnerPageLayout.jsx
@@ -3,33 +3,42 @@ import { Box, useMediaQuery } from "@chakra-ui/react";
 import InnerPageLeft from "./InnerPageLeft";
 import InnerPageRight from "./InnerPageRight";
 import MobileNav from "./MobileNav";
+import Div100vh from "react-div-100vh";
 
 const InnerPageLayout = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [isSmallerThan768] = useMediaQuery("(max-width: 767px)");
 
   return (
-    <Box
-      // display={["block", null, "grid"]}
-      // gridTemplateColumns="1.5fr 8.5fr"
-      overflowX="hidden"
-      w="100vw"
-      h="100vh"
-      position="relative"
-    >
-      {isSmallerThan768 ? (
-        <MobileNav sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-      ) : (
-        <InnerPageLeft
+    <Div100vh>
+      <Box
+        // display={["block", null, "grid"]}
+        // gridTemplateColumns="1.5fr 8.5fr"
+        overflowX="hidden"
+        w="100vw"
+        h="full"
+        position="relative"
+      >
+        {isSmallerThan768 ? (
+          <MobileNav
+            sidebarOpen={sidebarOpen}
+            setSidebarOpen={setSidebarOpen}
+          />
+        ) : (
+          <InnerPageLeft
+            sidebarOpen={sidebarOpen}
+            setSidebarOpen={setSidebarOpen}
+          />
+        )}
+
+        <InnerPageRight
           sidebarOpen={sidebarOpen}
           setSidebarOpen={setSidebarOpen}
-        />
-      )}
-
-      <InnerPageRight sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}>
-        {children}
-      </InnerPageRight>
-    </Box>
+        >
+          {children}
+        </InnerPageRight>
+      </Box>
+    </Div100vh>
   );
 };
 
